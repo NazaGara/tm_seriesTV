12c6c3726b527e56b206cbd0729fc08fba277c4e
nazagara <nazagara1277@gmail.com>
2021-11-01 13:37:36 -0300

    upload a corpus with 10k tweets

--- project.py
+++ project.py
@@ -12,47 +12,6 @@ import unicodedata
 CONSUMER_KEY = os.environ['TW_API_KEY']
 CONSUMER_SECRET = os.environ['TW_API_SECRET_KEY']
 BEARER_TOKEN = os.environ['TW_BEARER_TOKEN']
-#auth = tw.AppAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-#api = tw.API(auth, wait_on_rate_limit=True)
-
-
-auth = tw.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-#auth.set_access_token(access_token, access_token_secret)
-api = tw.API(auth, wait_on_rate_limit=True)
-
-search_words = "game of thrones"
-date_since = "201904010000"
-date_until = "202004010000"
-
-#tweets = api.search_30_day(label='dev', query=search_words, fromDate=date_since, toDate=date_until, maxResults=5)
-
-#tweets = (api.search_full_archive(label='dev', query=search_words, fromDate=date_since, toDate=date_until, maxResults=10))
-#print(tweets[0])
-
-""" 
-with open('tweets.txt', 'w') as f:
-    tweets = api.search_full_archive(label='dev', query=search_words, fromDate=date_since, toDate=date_until, maxResults=10)
-    for t in tweets:
-            f.write(t.text)
-            #json.dump(t, outfile)
-    for i in range(3):
-        tweets = api.search_full_archive(label='dev', query=search_words, fromDate=date_since, toDate=date_until, maxResults=20)
-        for t in tweets:
-            f.write(t.text)
-            #json.dump(t, outfile)
-            #print(t.text, t.created_at)
- """
-
-
-client = tw.Client(bearer_token=BEARER_TOKEN)
-
-# Replace with your own search query
-
-
-#Para poder acceder a all tweet necesito academic research, si no, no me deja
-#tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=100)
-# tweets = client.search_all_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=100)
-
 
 import re
 def remove_emoji(string):
@@ -70,10 +29,76 @@ def remove_accents(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
     return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
+def extract_tweets(input_file, output_file):
+    with open(output_file, 'a+') as f:
+        # JSON file
+        r = open(input_file, "r")
+        
+        # Reading from file
+        Lines = r.readlines()
+        count = 0
+        # Strips the newline character
+        for line in Lines:
+            count += 1
+            data = json.loads(line)
+            f.write(remove_emoji(remove_accents(data['text']))+"\n")
+    open(input_file, 'w').close()
+
+def pass_tweets(input_file, output_file='tweets.txt'):
+    with open(input_file, 'r+') as f:
+        fileoutput = f.readlines()
+        f.truncate(0)
+    with open(output_file, "a+") as f1:
+        for line in fileoutput:
+            f1.write(line)
+            f1.flush()
+
+
 # context_annotatios tienen la forma: (como se repiten no lo voy a tomar)
 # "context_annotations": [{"domain": {"id": "3", "name": "TV Shows",
 # "description": "Television shows from around the world"},
 # "entity": {"id": "1437413779755597824", "name": "Squid Game"}}]}
+
+# ------------------------------------------------------------------------------------------
+
+
+
+auth = tw.AppAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+def get_old_tweets(query, file_name, since, until, limit=1000):
+    with open(file_name, 'a+') as f:
+        for t in tw.Cursor(api.search_full_archive, label='dev', query=query, fromDate=since, toDate=until, maxResults=1000).items(limit):
+            text = remove_emoji(remove_accents(t.text.replace('\n',' ')))
+            data = {
+                "text": text,
+                "created_at": str(t.created_at),
+                "id": t.id,
+            }
+            json.dump(data, f)
+            f.write("\n")
+
+query = '-(RT) juego calamar lang:es -(has:media)'
+date_since = "202101010000"
+date_until = "202110150000"
+file_name = "old_tweets.txt"
+get_old_tweets(query, file_name, date_since, date_until, limit=10000)
+extract_tweets(file_name,"old_tweets_new.txt")
+pass_tweets("old_tweets_new.txt", "old_tweets.txt")
+
+
+
+# ------------------------------------------------------------------------------------------
+
+auth = tw.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+#auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+client = tw.Client(bearer_token=BEARER_TOKEN)
+
+
+
+# Buscar con Hashtags 
+
 query = 'juego calamar lang:es -(has:media) -is:retweet'
 file_name = 'tweets_squid2.txt'
 # Name and path of the file where you want the Tweets written to
@@ -93,32 +118,6 @@ def get_tweets(query, file_name, limit=10):
             f.write("\n")
 
 
-def extract_tweets(input_file, output_file):
-    with open(output_file, 'a+') as f:
-        # JSON file
-        r = open(input_file, "r")
-        
-        # Reading from file
-        Lines = r.readlines()
-        count = 0
-        # Strips the newline character
-        for line in Lines:
-            count += 1
-            data = json.loads(line)
-            f.write(remove_emoji(remove_accents(data['text']))+"\n")
-    open(input_file, 'w').close()
-
-def pass_tweets(input_file,output_file='tweets.txt'):
-    with open(input_file, 'r+') as f:
-        fileoutput = f.readlines()
-        f.truncate(0)
-    with open(output_file, "a+") as f1:
-        for line in fileoutput:
-            f1.write(line)
-            f1.flush()
-
-
-
-get_tweets(query, file_name, limit=1000)
-extract_tweets(file_name,"tweets_new.txt")
-pass_tweets("tweets_new.txt")
+#get_tweets(query, file_name, limit=100)
+#extract_tweets(file_name,"tweets_new.txt")
+#pass_tweets("tweets_new.txt")
